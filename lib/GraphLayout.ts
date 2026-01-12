dcb248282248065790475c22048b3c580cde6a26
sondrefossaa <sondrefossaa@gmail.com>
2026-01-12 18:15:43 +0100

    fixed vertical positioning bug

--- lib/GraphLayout.ts
+++ lib/GraphLayout.ts
@@ -8,7 +8,7 @@ const edgePadding: number = 10;
 
 
 function calculateNodePositions(nodesByLayer, width, height) {
-  const layers = Array.from(nodesByLayer.keys()).sort()
+  const layers = Array.from(nodesByLayer.keys()).sort((a, b) => a - b);
   let maxLayerSize = 0;
   for (let layer of layers) {
     const currLayer = nodesByLayer.get(layer);
@@ -21,19 +21,22 @@ function calculateNodePositions(nodesByLayer, width, height) {
     })
   }
 
-  const horizontalSpacing = width / (layers.length - 2);
+  const horizontalSpacing = width / (layers.length - 1);
   const verticalSpacing = (height - edgePadding * 2) / maxLayerSize;
 
   for (let layer of layers) {
+    console.log("Currentlayer: ", layer)
     if (layer === 0 || layer === -1) continue;
     const currLayer = nodesByLayer.get(layer);
-    const layerSizeMid = Math.floor(currLayer.length / 2);
+    const layerSizeMid = (currLayer.length - 1) / 2;
     currLayer.forEach((node, index) => {
-      node.currPos = { x: horizontalSpacing * layer, y: (index - layerSizeMid) * verticalSpacing };
+      console.log("layer: ", layer, "Horizontal spacing: ", horizontalSpacing, "Screen widht", width)
+      node.currPos = { x: horizontalSpacing * layer, y: (height / 2) + (index - layerSizeMid) * verticalSpacing };
+      console.log("Pos: ", node.currPos, "Values :", horizontalSpacing * layer, (index - layerSizeMid) * verticalSpacing)
     })
   }
-  nodesByLayer.get(0)[0].currPos = { x: edgePadding + nodeRadius, y: height / 2 };
-  nodesByLayer.get(-1)[0].currPos = { x: width - edgePadding - nodeRadius, y: height / 2 };
+  nodesByLayer.get(0)[0].currPos = { x: edgePadding + nodeRadius, y: (height - edgePadding) / 2 };
+  nodesByLayer.get(-1)[0].currPos = { x: width - edgePadding - nodeRadius, y: (height - edgePadding) / 2 };
 
   const result = [];
   for (let layer of layers) {
